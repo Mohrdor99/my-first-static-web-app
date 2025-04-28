699320d01991fde16508788a22fd85ee48b5bf4d
Marvin Mohr <marvin.mohr2@haw-hamburg.de>
2025-04-28 17:20:54 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hallo, das ist hier ist eine statische Web-App nach dem Tutorial</div>`,
 })
 export class AppComponent {
   value = 'World';
